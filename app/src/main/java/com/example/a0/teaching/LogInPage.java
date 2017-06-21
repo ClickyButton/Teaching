c52334e5e1c847f83ed4500ce1f7812e2b2cf5b6
othman alamoud <othmanalamoud@gmail.com>
2017-06-21 04:50:17 +0300

    LogInPage Modified

--- app/src/main/java/com/example/a0/teaching/LogInPage.java
+++ app/src/main/java/com/example/a0/teaching/LogInPage.java
@@ -1,13 +1,25 @@
 package com.example.a0.teaching;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class LogInPage extends AppCompatActivity {
+
+    Button b1, b2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in_page);
+
+        b1 = (Button) findViewById(R.id.button);
+        b2 = (Button) findViewById(R.id.button2);
+
+        Typeface cairo = Typeface.createFromAsset(getAssets(),"Cario-Regular.ttf");
+
+        b1.setTypeface(cairo);
+        b2.setTypeface(cairo);
     }
 }
